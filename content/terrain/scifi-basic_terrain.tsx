bcbdf07f53a6039a8a36e816fd5c5520d3e4de60
Drew M <58413506+DrewMalette@users.noreply.github.com>
2020-05-09 11:29:09 -0500

    Delete scifi-basic_terrain.tsx

--- content/terrain/scifi-basic_terrain.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.3" name="scifi-basic_terrain" tilewidth="32" tileheight="32" tilecount="100" columns="10">
- <image source="scifi-basic_terrain.png" trans="ff00ff" width="320" height="320"/>
-</tileset>
